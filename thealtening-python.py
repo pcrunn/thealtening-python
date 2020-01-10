497deddf55099fada0c577b4abd5c62b9a8327bb
Alexander P <pcrunn@pm.me>
2020-01-10 07:51:03 +0200

    clean code

--- thealtening-python.py
+++ thealtening-python.py
@@ -1,4 +1,12 @@
-import requests
-x = "http://api.thealtening.com/v1/generate?token=%enter-api-key-here%"
-r = requests.post(x)
-print(r.text)
+from requests import post
+from sys import argv
+
+if len(argv) < 2:
+  print('You must provide your api key')
+  exit()
+  
+api_key = argv[1]
+endpoint = 'http://api.thealtening.com/v1/generate?token=%s' % api_key
+response = post(endpoint)
+
+print(response.text)
